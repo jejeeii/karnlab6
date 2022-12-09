f9e0d5c20cbf202ae65dec03383d62ce807d0a17
chotirat <jejeshowka@gmail.com>
2022-12-09 15:59:18 +0700

    Filled out README

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,19 @@
 using namespace std;
 
 int main(){
+    int x = 1;
+    int y = 0;
+    int z = 0;
+    while( x != 0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> x;
+    if(x%2 == 0){
+    y++;
+    }else{
+    z++;
+    }
+    }
+    cout << "#Even numbers = " << y-1 << "\n";
+    cout << "#Odd numbers = " << z ;
     return 0;
 }
